9f718fe39f5840c345997c5c83eb789b76af636e
HoaHo <duchoactk36@gmail.com>
2023-02-13 10:50:46 +0700

    rename category table

--- BE/TechFix.Models/DataContext.cs
+++ BE/TechFix.Models/DataContext.cs
@@ -190,5 +190,6 @@ namespace TechFix.EntityModels
         public DbSet<ProductUnit> ProductUnits { get; set; }
         public DbSet<Store> Stores { get; set; }
         public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
