04a33203b98a057c49505e64d9a8fd78457bec96
thanhtrinh997 <thanhtrinh5083@gmail.com>
2023-02-27 23:23:45 +0700

    update thêm Id trả về cho Suppliers

--- BE/TechFix.TransportModels/Dtos/SupplierDto.cs
+++ BE/TechFix.TransportModels/Dtos/SupplierDto.cs
@@ -11,6 +11,7 @@ namespace TechFix.TransportModels.Dtos
 {
     public class SupplierDto : IMapFrom<Supplier>
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Phone { get; set; }
         public string Address { get; set; }
