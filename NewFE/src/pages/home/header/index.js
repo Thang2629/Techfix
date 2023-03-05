a7e550d7e52ffb18de88eddc591c528ecdaacccb
Quan Nguyen <quan.nguyen@trecs.sg>
2023-03-06 00:06:23 +0700

    add select store

--- NewFE/src/pages/home/header/index.js
+++ NewFE/src/pages/home/header/index.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import { Menu, Row, Col, Button, Dropdown } from "antd";
+import { Menu, Row, Col, Button, Dropdown, Space, Select } from "antd";
 import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
 import { fowardTo } from "utils/common/route";
 import { cleanUp } from "utils/storage";
 import { USER_KEY, SESSION_KEY } from "static/Constants";
 import { LoginOutlined, LogoutOutlined, UserOutlined } from "@ant-design/icons";
-
+import { getProductAssicatedByType } from "services/ProductAssociated";
+import { PRODUCT_ASSOCIATED, STORE_ID_KEY } from "static/Constants";
+import { useLocalStorage } from "hooks/localStorage";
 import "./header.less";
 
-const Header = (props) => {
+const HeaderProject = (props) => {
   const { handleCollapseSidebar, collapsed } = props;
-
+  const [stores, setStores] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [storeId, setStoreId] = useLocalStorage(STORE_ID_KEY, {});
   const logout = () => {
     cleanUp([USER_KEY, SESSION_KEY]);
     fowardTo("/login");
@@ -28,7 +32,9 @@ const Header = (props) => {
         break;
     }
   };
-
+  const HandleChangeStore = (value) => {
+    setStoreId(value);
+  };
   const menuItems = [
     {
       label: "Thông tin user",
@@ -50,6 +56,21 @@ const Header = (props) => {
     />
   );
 
+  useEffect(() => {
+    const initialize = async () => {
+      setIsLoading(true);
+      const response = await getProductAssicatedByType(
+        PRODUCT_ASSOCIATED.STORE
+      );
+
+      const storeId = JSON.parse(localStorage.getItem(STORE_ID_KEY));
+      if (!storeId) setStoreId(response[0].Id);
+      setStores(response);
+      setIsLoading(false);
+    };
+    initialize();
+  }, []);
+
   return (
     <div className="header">
       <Row className="wrapper" justify="space-between">
@@ -58,6 +79,32 @@ const Header = (props) => {
           icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
         />
         <Col>
+          <Dropdown overlay={dropdownMenu} trigger={["click"]}>
+            <Button
+              className="header__btn"
+              type="outlined"
+              icon={<UserOutlined />}
+            >
+              Hello! User
+            </Button>
+          </Dropdown>
+        </Col>
+        <Col style={{ display: "flex" }}>
+          <Select
+            style={{ width: 200 }}
+            loading={isLoading}
+            onChange={HandleChangeStore}
+            defaultValue={storeId}
+          >
+            {stores &&
+              stores.map((item) => {
+                return (
+                  <Select.Option key={`item_${item.Id}`} value={item.Id}>
+                    {item.Name}
+                  </Select.Option>
+                );
+              })}
+          </Select>
           <Dropdown menu={dropdownMenu} trigger={["click"]}>
             <Button
               className="header__btn"
@@ -73,7 +120,7 @@ const Header = (props) => {
   );
 };
 
-Header.propTypes = {
+HeaderProject.propTypes = {
   /**
    * options data for ribbon
    */
@@ -81,4 +128,4 @@ Header.propTypes = {
   optionType: PropTypes.string,
 };
 
-export default Header;
+export default HeaderProject;
